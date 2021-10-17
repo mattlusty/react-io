50217e045d8874b3662500ba83b839c3f712ad0e
Mattthew Lusty <mattlusty123@gmail.com>
2021-10-17 23:38:29 +0100

    custom-scrollbars-react 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import { Scrollbars } from "react-custom-scrollbars";
 //
 import "./styles/css/general.css";
 import "./styles/css/HamLogo.css";
