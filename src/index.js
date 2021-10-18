872fe4ae9395a80ff3763617a04651240169aeb4
Mattthew Lusty <mattlusty123@gmail.com>
2021-10-18 13:18:44 +0100

    scrollbar 4.3

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "overlayscrollbars/css/OverlayScrollbars.css";
-import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
 
 ReactDOM.render(
-  <OverlayScrollbarsComponent style={{ width: "100%", height: "100%" }}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </OverlayScrollbarsComponent>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
